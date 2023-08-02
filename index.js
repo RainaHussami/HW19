57685c249557fe5c9aa981120821891d8296d5de
RainaHussami <127688268+RainaHussami@users.noreply.github.com>
2023-08-02 19:44:33 -0400

    commit

---
+++ index.js
@@ -0,0 +1,96 @@
+alert ("Welcome!")
+  $(document).ready(function() {
+    $("#scroll1").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".png").offset().top
+        },
+        1000 
+      );
+    });
+  });
+  
+  $(document).ready(function() {
+    $("#scroll2").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".lastsect").offset().top
+        },
+        1000
+      );
+    });
+  });
+  
+  $(document).ready(function() {
+    $("#scroll3").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".mywork").offset().top
+        },
+        1000 
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    $("#scroll4").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".footer").offset().top
+        },
+        1000 
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    $(".about-me-button").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".p2").offset().top
+        },
+        1000 
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    $("#footer1").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".p2").offset().top
+        },
+        1000 
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    $("#footer2").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".lastsect").offset().top
+        },
+        1000 
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    $("#footer3").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".mywork").offset().top
+        },
+        1000 
+      );
+    });
+  });
